761792a801d8c7cb9893d3b39e3e229cdc6bbd2a
Doucharm <popcorn19122001@gmail.com>
2024-04-18 12:36:26 +0200

    credentials add

--- main.py
+++ main.py
@@ -2,27 +2,33 @@
 from selenium.webdriver import Edge, EdgeOptions
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
-
 import json
 from variables import data_source
+import time
 def log_in_hanlder(driver,credentials):
-    elem = driver.find_element(By.ID, "userNameInput")
-    elem.clear()
-    elem.send_keys(str(credentials['email']))
-    elem.send_keys(Keys.RETURN)
-    elem = driver.find_element(By.ID, "passwordInput")
-    elem.clear()
-    elem.send_keys(str(credentials['password']))
-    elem.send_keys(Keys.RETURN)
-    elem = driver.find_element(By.ID, "submitButton")
-    elem.click()
+    username_field = driver.find_element(By.NAME, "Username")
+    username_field.clear()
+    username_field.send_keys(credentials['email'])
+    time.sleep(1)
+    
+    password_field = driver.find_element(By.NAME, "Password") 
+    password_field.clear()
+    password_field.send_keys(credentials['password'])
+    time.sleep(1)
+    
+    password_field.send_keys(Keys.RETURN)
+    time.sleep(5)
+    print("Finish logging in")
 def get_data_online(url,credentials) -> list:
     options = EdgeOptions()
     options.headless = True
     driver = Edge(options=options)
     driver.get(url)
-    elem = driver.find_element(By.ID, "userNameInput")
     driver.refresh()
+    try:
+        log_in_hanlder(driver=driver,credentials=credentials)
+    except:
+        print(Exception)
     #attempt to collect data from webpage
     data = driver.page_source
     driver.quit()
@@ -45,9 +51,14 @@ def data_format(data):
     finally:
         print('Successfully format data to workable format')
         return formated_data
+def store_data(data):
+   with open("data.json", "w") as f:
+        json.dump(data, f, indent=4)
 def main():
-    credentials={'email':'randomemail@unob.cz','password':'12345'}
+    from credentials import get_credentials
+    credentials=get_credentials()
     data = get_data_online(data_source,credentials=credentials)
+    store_data(data=data)
     print(data)
 if __name__ == '__main__':
     main()
--- variables.py
+++ variables.py
@@ -1,2 +1,2 @@
 import os
-data_source=os.environ.get('DATA_SOURCE','https://apl.unob.cz/planovanivyuky/api/read/atributy')
+data_source=os.environ.get('DATA_SOURCE','https://apl.unob.cz/rozvrh/api/read/rozvrh?id=7')
