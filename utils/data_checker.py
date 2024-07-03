9014ae6be1535d9970ae7efc61da0b206e5880d4
Doucharm <popcorn19122001@gmail.com>
2024-07-03 08:26:59 +0200

    Updating with real data ( ignored)

--- utils/data_checker.py
+++ utils/data_checker.py
@@ -1,19 +1,13 @@
-from sqlalchemy import select
-def consistency_classification(classification,type_classification):
-    if type_classification not in ['Z','z','zapocet','započet']:
-        if classification not in ['A','B','C','D','E','F']:
-            return False
-    return True
-def consistency_group(group):
-    sample=group[0]
-    for student in group:
-        if sample['type_classification'] != student['type_classification']:
-            return False
-    return True
 def unique_instance(list):
     data= []
     for item in list:
-        if item not in data:
+        item_important={
+            'semester_id': item['semester_id'],
+            'student_id' : item['student_id'],
+            "classificationlevel_id" : item[ "classificationlevel_id"],
+            "order": item['order']
+        }
+        if item_important not in data:
             data.append(item)
     print('Number of duplicated instances',len(list)-len(data))
     return data
--- utils/generator_classification.py
+++ utils/generator_classification.py
@@ -6,49 +6,75 @@ def generate_classification(acclassificationtypes):
             {"id": "5fae9dd7-b095-11ed-9bd8-0242ac110002" , "name": "Nezapoctovano", "name_en": "Failed"}]
     else:
         pool=[
-            {"id": "5fae9dd8-b095-11ed-9bd8-0242ac110002" , "name": "A", "name_en": "A"},
-            {"id": "5faea134-b095-11ed-9bd8-0242ac110002" , "name": "B", "name_en": "B"},
-            {"id": "5faea21a-b095-11ed-9bd8-0242ac110002" , "name": "C", "name_en": "C"},
-            {"id": "5faea2d8-b095-11ed-9bd8-0242ac110002" , "name": "D", "name_en": "D"},
-            {"id": "5faea332-b095-11ed-9bd8-0242ac110002" , "name": "E", "name_en": "E"},
-            {"id": "5faea396-b095-11ed-9bd8-0242ac110002" , "name": "F", "name_en": "F"}] 
+            {"id": "5fae9dd8-b095-11ed-9bd8-0242ac110002", "name": "A", "name_en": "A"},
+            {"id": "5faea134-b095-11ed-9bd8-0242ac110002", "name": "B", "name_en": "B"},
+            {"id": "5faea21a-b095-11ed-9bd8-0242ac110002", "name": "C", "name_en": "C"},
+            {"id": "5faea2d8-b095-11ed-9bd8-0242ac110002", "name": "D", "name_en": "D"},
+            {"id": "5faea332-b095-11ed-9bd8-0242ac110002", "name": "E", "name_en": "E"},
+            {"id": "5faea396-b095-11ed-9bd8-0242ac110002", "name": "F", "name_en": "F"}
+            ]
     return random.choice(pool)
-def generate_order():
+def generate_order(student_id,semester_id,classificationtype_id):
+    print('here',classificationtype_id)
     rand_num = random.randint(0, 99)
     if rand_num < 90:  # 90% probability
-        return 1
+        return [
+            new_classification(student_id,semester_id,classificationtype_id,order=1)
+        ]
     elif rand_num < 99:  # 9% probability
-        return 2
+        return [
+            new_classification(student_id,semester_id,classificationtype_id,order=1),
+            new_classification(student_id,semester_id,classificationtype_id,order=2)
+        ]
     else:  # 1% probability 
-        return 3
-def new_classification(student_id,semester_id,classificationtype_id):
+        return [
+            new_classification(student_id,semester_id,classificationtype_id,order=1),
+            new_classification(student_id,semester_id,classificationtype_id,order=2),
+            new_classification(student_id,semester_id,classificationtype_id,order=3)
+        ]
+def new_classification(student_id,semester_id,classificationtype_id,order):
+    print(classificationtype_id,'printee')
     new_classification = {
             "id": str(uuid.uuid4()),  # Generate a new unique ID 
             "student_id": student_id,
-            "semester_id": semester_id,
+            "semester_id":  semester_id,
             "classificationlevel_id": generate_classification(classificationtype_id)['id'],
-            "order":generate_order(),
+            "order":order,
             "date": str(datetime.datetime.now())
             }
     return new_classification
-with open('systemdata.json', 'r', encoding='utf-8') as f:
+with open('real_data.json', 'r', encoding='utf-8') as f:
     data = json.load(f)
 generated_classification = []
 
+
 for group in data['groups']:
-    if group['grouptype_id'] == "cd49e157-610c-11ed-9312-001a7dda7110":
-        students_id = [m['user_id'] for m in data['memberships'] if m['group_id'] == group['id']]
+    if group['grouptype_id'] == "cd49e157-610c-11ed-9312-001a7dda7110": #check if a group is study group 
+        students_id = [m['user_id'] for m in data['memberships'] if m['group_id'] == group['id']] #retrive all student inside a study group
         if not students_id:
             continue
-        program_id = next(program['program_id'] for program in data['acprograms_students'] if program['student_id'] == students_id[0])
-        subjects = [subject for subject in data['acsubjects'] if subject['program_id'] == program_id]
-        for subject in subjects:
-            semester = next(semester for semester in data['acsemesters'] if semester['subject_id'] == subject['id'])
-            classificationtype_id = semester['classificationtype_id']
-            generated_classification.extend([new_classification(semester_id=semester['id'], classificationtype_id=classificationtype_id, student_id=student) for student in students_id])
-with open('classification.json', 'w', encoding='utf-8') as f:
+        program_id=None
+        for program in data['acprograms_students']:
+            if program['student_id'] == students_id[0]:
+                program_id=program['program_id']
+        if not program_id:
+            continue
+        for subject in data['acsubjects']:
+            if subject['program_id'] == program_id:
+                for semester in data['acsemester']:
+                    if semester['subject_id'] == subject['id']:
+                        classificationtype_id = semester['classificationtype_id']
+                        semester_id=semester['id']
+                        print(semester_id,"+",classificationtype_id)
+                        for student in students_id:
+                            generated_classification.extend(generate_order(semester_id=semester_id, classificationtype_id=classificationtype_id, student_id=student))
+from data_checker import unique_instance
+temp=unique_instance(generated_classification)
+
+with open('classification_official.json', 'w', encoding='utf-8') as f:
     json.dump(generated_classification, f, ensure_ascii=False, indent=4)
 
+            
             
 
                 
